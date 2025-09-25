63c8e9c9a8f63a89b4bf95b393422dc0d14e1f49
Mark Tabatskyi <mark.tabatskyi@gmail.com>
2025-09-25 22:37:24 +0300

    change structure

---
+++ compiler.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <unordered_map>
+#include <regex>
+#include <cctype>
+
+using std::string; using std::string_view;
+
+int main(int argc, char** argv) 
+{
+    std::ifstream fin(argv[1]); 
+    if(!fin)
+    { 
+        std::cerr<<"Cannot open file\n"; 
+        return 1; 
+    }
+
+    std::string line; 
+    int lineNo=0; 
+    bool sawReturn=false;
+
+    std::regex declRe(R"(^\s*var\s+([A-Za-z_][A-Za-z0-9_]*)\s*$)");
+    std::regex assignRe(R"(^\s*([A-Za-z_][A-Za-z0-9_]*)\s*=\s*(.+?)\s*$)");
+    std::regex returnRe(R"(^\s*return\s+([A-Za-z_][A-Za-z0-9_]*)\s*$)");
+
+    while(std::getline(fin,line))
+    {
+        ++lineNo; 
+        if(line.find_first_not_of(" \t\r\n")==std::string::npos) continue;
+        std::smatch m;
+
+        if(std::regex_match(line,m,declRe))
+        {
+            if(sawReturn) throw std::runtime_error("Statements after return at line " + std::to_string(lineNo));
+        }
+        if(std::regex_match(line,m,assignRe))
+        {
+            if(sawReturn) throw std::runtime_error("Statements after return at line " + std::to_string(lineNo));
+            {
+                string var = m[1]; string exprTxt = m[2];
+            
+
+            }
+        }
+        if(std::regex_match(line,m,returnRe))
+        {
+            if(sawReturn) throw std::runtime_error("Multiple return statements (line " + std::to_string(lineNo) + ")");
+            
+        }
+        throw std::runtime_error("Syntax error line " + std::to_string(lineNo)+": " + line);
+    }
+    if(!sawReturn) 
+    { 
+        std::cerr<<"Error: no return statement\n"; 
+        return 2; 
+    }
+
+    struct Var { string allocaName; };
+    struct IRContext {
+        std::unordered_map<string,Var> vars;
+        int tempId = 0;
+        std::ostringstream ir;
+    } ctx;
+    
+    ctx.ir << "declare i32 @printf(i8*, ...)\n\n";
+    ctx.ir << "@fmt = private constant [31 x i8] c\"Program exit with result %d\\0A\\00\"\n\n";
+    ctx.ir << "define i32 @main() {\n";
+    
+    ctx.ir << "}\n";
+    return 0;
+}
