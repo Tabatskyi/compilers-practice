69add1ece6831c7a8112d0eb9966851a3661c823
Mark Tabatskyi <mark.tabatskyi@gmail.com>
2025-09-26 13:54:09 +0300

    smth works

--- compiler.cpp
+++ compiler.cpp
@@ -7,7 +7,8 @@
 #include <regex>
 #include <cctype>
 
-using std::string; using std::string_view;
+using std::string; 
+using std::string_view;
 
 int main(int argc, char** argv) 
 {
@@ -18,6 +19,8 @@ int main(int argc, char** argv)
         return 1; 
     }
 
+    std::string filename = argv[1];
+    filename = filename.substr(0, filename.find_last_of('.'));
     std::string line; 
     int lineNo=0; 
     bool sawReturn=false;
@@ -25,38 +28,6 @@ int main(int argc, char** argv)
     std::regex declRe(R"(^\s*var\s+([A-Za-z_][A-Za-z0-9_]*)\s*$)");
     std::regex assignRe(R"(^\s*([A-Za-z_][A-Za-z0-9_]*)\s*=\s*(.+?)\s*$)");
     std::regex returnRe(R"(^\s*return\s+([A-Za-z_][A-Za-z0-9_]*)\s*$)");
-
-    while(std::getline(fin,line))
-    {
-        ++lineNo; 
-        if(line.find_first_not_of(" \t\r\n")==std::string::npos) continue;
-        std::smatch m;
-
-        if(std::regex_match(line,m,declRe))
-        {
-            if(sawReturn) throw std::runtime_error("Statements after return at line " + std::to_string(lineNo));
-        }
-        if(std::regex_match(line,m,assignRe))
-        {
-            if(sawReturn) throw std::runtime_error("Statements after return at line " + std::to_string(lineNo));
-            {
-                string var = m[1]; string exprTxt = m[2];
-            
-
-            }
-        }
-        if(std::regex_match(line,m,returnRe))
-        {
-            if(sawReturn) throw std::runtime_error("Multiple return statements (line " + std::to_string(lineNo) + ")");
-            
-        }
-        throw std::runtime_error("Syntax error line " + std::to_string(lineNo)+": " + line);
-    }
-    if(!sawReturn) 
-    { 
-        std::cerr<<"Error: no return statement\n"; 
-        return 2; 
-    }
 
     struct Var { string allocaName; };
     struct IRContext {
@@ -66,9 +37,60 @@ int main(int argc, char** argv)
     } ctx;
     
     ctx.ir << "declare i32 @printf(i8*, ...)\n\n";
-    ctx.ir << "@fmt = private constant [31 x i8] c\"Program exit with result %d\\0A\\00\"\n\n";
+    ctx.ir << "@fmt = private constant [29 x i8] c\"Program exit with result %d\\0A\\00\"\n\n";
     ctx.ir << "define i32 @main() {\n";
     
+
+    while (std::getline(fin, line)) {
+        ++lineNo;
+        if (line.find_first_not_of(" \t\r\n")==std::string::npos) continue;
+        std::smatch m;
+        if (std::regex_match(line, m, declRe)) {
+            if(sawReturn) throw std::runtime_error("Statements after return at line " + std::to_string(lineNo));
+            {
+                string var = m[1]; string exprTxt = m[2];
+                if(ctx.vars.count(var)) throw std::runtime_error("Variable re-declaration at line " + std::to_string(lineNo));
+                ctx.ir << "  %" << var << " = alloca i32\n";
+                ctx.vars[var] = Var{var};
+                continue;
+            }
+        }
+        if (std::regex_match(line, m, assignRe)) {
+            if(sawReturn) throw std::runtime_error("Statements after return at line " + std::to_string(lineNo));
+            {
+                auto var = m[1];
+                auto expr = m[2];
+                if(!ctx.vars.count(var)) throw std::runtime_error("Undeclared variable at line " + std::to_string(lineNo));
+                ctx.ir << "  store i32 " << expr << ", i32* %" << var << "\n";
+                continue;
+            }
+        }
+        if (std::regex_match(line, m, returnRe)) {
+            if(sawReturn) throw std::runtime_error("Multiple return statements (line " + std::to_string(lineNo) + ")");
+            sawReturn = true;
+            {
+                auto var = m[1];
+                ctx.ir << "  %retv = load i32, i32* %" << var << "\n";
+                ctx.ir << "  %fmtptr = getelementptr [31 x i8], [31 x i8]* @fmt, i32 0, i32 0\n";
+                ctx.ir << "  call i32 (i8*, ...) @printf(i8* %fmtptr, i32 %retv)\n";
+                ctx.ir << "  ret i32 %retv\n";
+                continue;
+            }
+        }
+        throw std::runtime_error("Syntax error line " + std::to_string(lineNo)+": " + line);
+    }
+    if(!sawReturn) 
+    { 
+        std::cerr<<"Error: no return statement\n"; 
+        return 2; 
+    }
+
     ctx.ir << "}\n";
+
+
+    std::ofstream fout(filename + ".ll");
+    fout << ctx.ir.str();
+    fout.close();
+
     return 0;
 }
